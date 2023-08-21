6130b34bfbb49e04c1f68f0e89cf980bc4224f0c
SimoneVilardo <simovila95@gmail.com>
2023-08-21 16:05:04 +0200

    funzione delete completata

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -22,10 +22,10 @@
                             <td class="bg-info fw-bold">
                                 <a class="btn btn-dark btn-sm" href="{{ route('admin.projects.show', $project->id) }}"><i class="fas fa-eye"></i></a>
                                 <a class="btn btn-warning btn-sm" href="{{ route('admin.projects.edit', $project->id) }}"><i class="fas fa-pen"></i></a>
-                                <form class="d-inline-block" action="#" onsubmit="return confirm('Sei sicuro di voler cancellare questo fumetto?')" method="POST">
+                                <form class="d-inline-block" action="{{ route('admin.projects.destroy', $project->id)}}" onsubmit="return confirm('Sei sicuro di voler cancellare questo project?')" method="POST">
                                     @csrf
                                     @method('DELETE')
-                                    <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
+                                    <button class="btn btn-danger btn-sm" type="submit"><i class="fas fa-trash"></i></button>
                                 </form>
                             </td>
                         </tr>

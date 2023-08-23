803c8634454bafc0d37dd238f49a5240518ec38d
SimoneVilardo <simovila95@gmail.com>
2023-08-23 13:40:01 +0200

    migration e seeder di image

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -8,7 +8,7 @@
                 <a href="{{ route('admin.projects.index') }}" class="btn btn-sm btn-primary">Ritorna alla lista completa</a>
             </div>
             <div class="col-12">
-                <form action="{{ route('admin.projects.store') }}" method="POST">
+                <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="form-group mt-4">
                         <label class="contol-lable">Titolo</label>
@@ -16,6 +16,10 @@
                         @error('title')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
+                    </div>
+                    <div>
+                        <label class="contol-lable">Immagine</label>
+                        <input class="form-control @error('image')is-invalid @enderror" type="file" name="image" id="image">
                     </div>
                     <div class="form-group mt-4">
                         <label class="contol-lable">Contenuto</label>
